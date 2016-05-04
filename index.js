3674576662a787d0c743c7d5092d86b76a2462ca
Tim Berners-Lee <timbl+git@w3.org>
2016-05-04 15:21:05 -0400

    mashlib using browserify

--- index.js
+++ index.js
@@ -11,34 +11,30 @@ requireState.loadedPackage['http'] = requireState.loadedPackage['http-browserify
 
 
 //  Solid-compatible UI module
-var UI = require('../solid-ui/index.js')
+var UI = require('solid-ui')
 
 var $rdf = UI.rdf
 $rdf.log = UI.log
 
 
-UI.OutlineObject  = require('../panes/outline/manager.js')
+UI.OutlineObject  = require('./panes/outline/manager.js')
 
 // later in the context of a window and a document:
-// dom.outline = new tabulator.OutlineObject(dom)
+
+var dom = window.document
+dom.outline = new tabulator.OutlineObject(dom)
 
 
-
-//Load the icons namespace onto tabulator.
-tabulator.loadScript("js/init/icons.js");
-//And Namespaces..
-// tabulator.loadScript("js/init/namespaces.js");
-//And Panes.. (see the below file to change which panes are included)
-tabulator.panes = UI.panes = require("../panes/index.js")
+UI.panes = require("./panes/index.js")
 
 
-// tabulator.loadScript("js/init/panes.js");
-// tabulator.loadScript("js/jscolor/jscolor.js");
-tabulator.panes.jscolor = UI.color = require('../jscolor/jscolor.js')
+// UI.color = require('jscolor/jscolor.js')
+
+
 //And Preferences mechanisms.
-tabulator.loadScript("js/init/prefs.js");
+// tabulator.loadScript("js/init/prefs.js");
 
-
+/*
 tabulator.loadScript("js/tab/sources-ext.js");
 
 //And, finally, all non-pane UI code.
@@ -50,5 +46,7 @@ tabulator.loadScript("js/tab/userinput.js"); // moved to panes
 
 //Oh, and the views!
 // tabulator.loadScript("js/init/views.js");
+
+*/
 
 // ENDS
