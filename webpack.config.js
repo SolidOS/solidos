a70c69ad630122a7f39b838176afd90f3e2235fa
Ruben Verborgh <ruben@verborgh.org>
2017-09-07 21:01:12 -0400

    Add development server.

--- webpack.config.js
+++ webpack.config.js
@@ -2,46 +2,51 @@ const path = require('path')
 const webpack = require('webpack')
 const MinifyPlugin = require('babel-minify-webpack-plugin')
 
-module.exports = {
-  entry: [
-    './index.js'
-  ],
-  output: {
-    path: path.join(__dirname, '/dist/'),
-    filename: 'mashlib.min.js',
-    library: 'Mashlib',
-    libraryTarget: 'umd'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/
-      },
-      {
-        test: /^.*\/solid-app-set\/.*\.js$/,
-        loader: 'babel-loader'
-      }
+module.exports = (env) => {
+  const development = env === 'development'
+
+  return {
+    entry: [
+      './index.js'
+    ],
+    output: {
+      path: path.join(__dirname, '/dist/'),
+      publicPath: '/dist/',
+      filename: 'mashlib.min.js',
+      library: 'Mashlib',
+      libraryTarget: 'umd'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          loader: 'babel-loader',
+          exclude: /node_modules/
+        },
+        {
+          test: /^.*\/solid-app-set\/.*\.js$/,
+          loader: 'babel-loader'
+        }
+      ]
+    },
+    plugins: [
+      new webpack.DefinePlugin({ 'global.IS_BROWSER': true })
+    ],
+    externals: {
+      'fs': 'null',
+      'node-fetch': 'fetch',
+      'isomorphic-fetch': 'fetch',
+      'xmldom': 'window',
+      'xmlhttprequest': 'XMLHttpRequest',
+      'xhr2': 'XMLHttpRequest',
+      'text-encoding': 'TextEncoder',
+      'whatwg-url': 'window',
+      '@trust/webcrypto': 'crypto',
+      'webcrypto': 'crypto'
+    },
+    devtool: 'source-map',
+    plugins: development ? [] : [
+      new MinifyPlugin()
     ]
-  },
-  plugins: [
-    new webpack.DefinePlugin({ 'global.IS_BROWSER': true })
-  ],
-  externals: {
-    'fs': 'null',
-    'node-fetch': 'fetch',
-    'isomorphic-fetch': 'fetch',
-    'xmldom': 'window',
-    'xmlhttprequest': 'XMLHttpRequest',
-    'xhr2': 'XMLHttpRequest',
-    'text-encoding': 'TextEncoder',
-    'whatwg-url': 'window',
-    '@trust/webcrypto': 'crypto',
-    'webcrypto': 'crypto'
-  },
-  devtool: 'source-map',
-  plugins: [
-    new MinifyPlugin()
-  ]
+  }
 }
