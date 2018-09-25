df6d8b9b600eba90c55139c9ec260b28c7a32b13
Ruben Verborgh <ruben@verborgh.org>
2018-09-25 17:05:21 +0200

    Simplify build configuration.

--- webpack.config.js
+++ webpack.config.js
@@ -2,100 +2,53 @@ const path = require('path')
 const webpack = require('webpack')
 const MinifyPlugin = require('babel-minify-webpack-plugin')
 
-module.exports = (env) => {
-  const development = env === 'development'
-
-  return [
-
-    {
-      mode: "development",
-      entry: [
-        './index.js'
-      ],
-      output: {
-        path: path.join(__dirname, '/dist/'),
-        publicPath: '/dist/',
-        filename: 'mashlib.js',
-        library: 'Mashlib',
-        libraryTarget: 'umd'
-      },
-      module: {
-        rules: [
-          {
-            test: /\.js$/,
-            loader: 'babel-loader',
-            exclude: /node_modules/
-          },
-          {
-            test: /^.*\/solid-app-set\/.*\.js$/,
-            loader: 'babel-loader'
-          }
-        ]
-      },
-
-      plugins: [
-        new webpack.DefinePlugin({ 'global.IS_BROWSER': true })
-          // new MinifyPlugin({ deadcode: false })
-      ],
-
-      externals: {
-        'fs': 'null',
-        'node-fetch': 'fetch',
-        'isomorphic-fetch': 'fetch',
-        'xmldom': 'window',
-        'xmlhttprequest': 'XMLHttpRequest',
-        'xhr2': 'XMLHttpRequest',
-        'text-encoding': 'TextEncoder',
-        'whatwg-url': 'window',
-        '@trust/webcrypto': 'crypto',
-        'webcrypto': 'crypto'
-      },
-      devtool: 'source-map'
+module.exports = (env, args) => {
+  const production = args.mode === 'production';
+  return {
+    mode: args.mode || 'development',
+    entry: [
+      './index.js'
+    ],
+    output: {
+      path: path.join(__dirname, '/dist/'),
+      publicPath: '/dist/',
+      filename: production ? 'mashlib.min.js' : 'mashlib.js',
+      library: 'Mashlib',
+      libraryTarget: 'umd'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          loader: 'babel-loader',
+          exclude: /node_modules/
+        },
+        {
+          test: /^.*\/solid-app-set\/.*\.js$/,
+          loader: 'babel-loader'
+        }
+      ]
     },
 
-    {
-      mode: "production",
-      entry: [
-        './index.js'
-      ],
-      output: {
-        path: path.join(__dirname, '/dist/'),
-        publicPath: '/dist/',
-        filename: 'mashlib.min.js',
-        library: 'Mashlib',
-        libraryTarget: 'umd'
-      },
-      module: {
-        rules: [
-          {
-            test: /\.js$/,
-            loader: 'babel-loader',
-            exclude: /node_modules/
-          },
-          {
-            test: /^.*\/solid-app-set\/.*\.js$/,
-            loader: 'babel-loader'
-          }
-        ]
-      },
-      plugins: [
-        new webpack.DefinePlugin({ 'global.IS_BROWSER': true }),
-        new MinifyPlugin({ deadcode: false })
-      ],
-      externals: {
-        'fs': 'null',
-        'node-fetch': 'fetch',
-        'isomorphic-fetch': 'fetch',
-        'xmldom': 'window',
-        'xmlhttprequest': 'XMLHttpRequest',
-        'xhr2': 'XMLHttpRequest',
-        'text-encoding': 'TextEncoder',
-        'whatwg-url': 'window',
-        '@trust/webcrypto': 'crypto',
-        'webcrypto': 'crypto'
-      },
-      devtool: 'source-map'
-    }
+    plugins: [
+      new webpack.DefinePlugin({ 'global.IS_BROWSER': true }),
+    ],
 
-  ]
+    optimization: {
+      minimizer: [
+        new MinifyPlugin({ deadcode: false }),
+      ]
+    },
+
+    externals: {
+      'fs': 'null',
+      'node-fetch': 'fetch',
+      'isomorphic-fetch': 'fetch',
+      'xmldom': 'window',
+      'text-encoding': 'TextEncoder',
+      'whatwg-url': 'window',
+      '@trust/webcrypto': 'crypto'
+    },
+    devtool: 'source-map'
+  }
 }
