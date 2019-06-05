807eebb019cf79ed129b785c80105dd6ecbe60f2
Vincent <git@vincenttunru.com>
2019-06-05 13:06:36 +0200

    Generate client-side bundle with Solid Style Guide
    To be able to incorporate styles from Inrupt's Solid Style Guide
    (see https://design.inrupt.com/), this commit introduces the
    generation of HTML and CSS files in addition to the Javascript
    files. This means that, instead of shipping their own HTML files,
    Pod servers that host the data browser should now simply serve up
    the `dist` directory in the Mashlib Node Module folder. This makes
    sure that relative references to the CSS file keep working.

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,18 @@
 const path = require('path')
 const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = (env, args) => {
   const production = args.mode === 'production';
   return {
     mode: args.mode || 'development',
     entry: [
-      './index.js'
+      './src/index.js'
     ],
     output: {
       path: path.join(__dirname, '/dist/'),
-      publicPath: '/dist/',
+      publicPath: '/',
       filename: production ? 'mashlib.min.js' : 'mashlib.js',
       library: 'Mashlib',
       libraryTarget: 'umd'
@@ -25,12 +27,42 @@ module.exports = (env, args) => {
         {
           test: /^.*\/solid-app-set\/.*\.js$/,
           loader: 'babel-loader'
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+              options: {
+                hmr: process.env.NODE_ENV === 'development',
+              },
+            },
+            'css-loader',
+            'sass-loader'
+          ],
+        },
+        {
+          test: /\.css$/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+              options: {
+                hmr: process.env.NODE_ENV === 'development',
+              },
+            },
+            'css-loader'
+          ],
         }
       ]
     },
 
     plugins: [
       new webpack.DefinePlugin({ 'global.IS_BROWSER': true }),
+      new HtmlWebpackPlugin({
+        title: 'Solid Data Browser',
+        template: './src/index.html'
+      }),
+      new MiniCssExtractPlugin({})
     ],
 
     externals: {
