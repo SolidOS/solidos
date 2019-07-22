9ab8a30cb53be5a019e2082d36c1b1c4d25672e5
Arne Hassel <arne.hassel@gmail.com>
2019-07-22 11:07:09 +0200

    Will now refresh header upon login/logout

--- src/global/header.ts
+++ src/global/header.ts
@@ -1,10 +1,12 @@
 import $rdf, { Fetcher, IndexedFormula, NamedNode, sym } from "rdflib"
 import namespace from "solid-namespace"
+import panes from "solid-panes"
 import { icon } from "./icon"
+import { SolidSession } from "../../typings/solid-auth-client"
 
 const ns = namespace($rdf)
 
-export async function initHeader (store: IndexedFormula, fetcher: Fetcher, profile: NamedNode | null) {
+export async function initHeader (store: IndexedFormula, fetcher: Fetcher) {
   const pod = getPod()
   const podOwnerProfile = getProfile(pod)
   await fetcher.load(podOwnerProfile)
@@ -12,36 +14,45 @@ export async function initHeader (store: IndexedFormula, fetcher: Fetcher, profi
   if (!header) {
     return
   }
+
+  panes.UI.authn.solidAuthClient.trackSession(function(session: SolidSession | null) {
+    const profile = session ? sym(session.webId) : null
+    header.innerHTML = ''
+    buildHeader(header, store, profile, pod, podOwnerProfile)
+  })
+}
+
+function buildHeader(header: HTMLElement, store: IndexedFormula, profile: NamedNode | null, pod: NamedNode, podOwnerProfile: NamedNode) {
   header.appendChild(createPodLink(pod))
   if (!profile || (profile && !profile.equals(podOwnerProfile))) {
     header.appendChild(createProfileLink(store, podOwnerProfile))
   }
 }
 
-function createPodLink(pod: NamedNode): HTMLElement {
-  const podLink = document.createElement('a')
+function createPodLink (pod: NamedNode): HTMLElement {
+  const podLink = document.createElement("a")
   podLink.href = pod.uri
-  podLink.classList.add('header-banner__link')
+  podLink.classList.add("header-banner__link")
   podLink.innerHTML = icon
 
-  const banner = document.createElement('div')
-  banner.classList.add('header-banner')
+  const banner = document.createElement("div")
+  banner.classList.add("header-banner")
   banner.appendChild(podLink)
 
   return banner
 }
 
-function createProfileLink(store: IndexedFormula, profile: NamedNode): HTMLElement {
-  const profileLinkPre = document.createElement('span')
+function createProfileLink (store: IndexedFormula, profile: NamedNode): HTMLElement {
+  const profileLinkPre = document.createElement("span")
   profileLinkPre.innerText = "You're visiting the Pod controlled by "
 
-  const profileLink = document.createElement('a')
+  const profileLink = document.createElement("a")
   profileLink.href = profile.uri
-  profileLink.classList.add('header-aside__link')
+  profileLink.classList.add("header-aside__link")
   profileLink.innerText = getName(store, profile)
 
-  const profileLinkContainer = document.createElement('aside')
-  profileLinkContainer.classList.add('header-aside')
+  const profileLinkContainer = document.createElement("aside")
+  profileLinkContainer.classList.add("header-aside")
   profileLinkContainer.appendChild(profileLinkPre)
   profileLinkContainer.appendChild(profileLink)
   return profileLinkContainer
